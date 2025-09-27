3fcf8309e64b0e968836297393834e2a4711edc7
junaid <junaid@junaids-MacBook-Air.local>
2025-09-27 21:34:03 +0530

    shopping cart commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 
 import './App.css'
+import Cart from './components/Cart'
 
 function App() {
     return (
     <h1 className="text-red-500">
       React 19 with Tailwind CSS v4
+      <Cart/>
     </h1>
   ) 
 }
---
+++ src/components/Cart.tsx
@@ -0,0 +1,12 @@
+
+
+
+function Cart() {
+    return (
+    <h1 className="text-red-500">
+      React 19 with Tailwind CSS v4
+    </h1>
+  ) 
+}
+
+export default Cart
